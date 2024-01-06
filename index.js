1b9b5004e794740c9ebc4849ded10f3c61d35500
Carles Andres <carlos.andres.c@gmail.com>
2024-01-06 22:04:03 +0000

    Added extra size and made .ico smaller

--- index.js
+++ index.js
@@ -3,18 +3,29 @@ import svgToImg from 'svg-to-img';
 import pngToIco from 'png-to-ico';
 import chalk from 'chalk';
 
+const savePng = async (file, size) => {
+  console.log(chalk.blue('Saving PNG size: ' + size));
+  const buffer = await svgToImg.from(file).toPng({ width: size, height: size });
+  console.log(chalk.blue('Saving PNG...'));
+  const fileName = `output-${size}.png`;
+  await fs.writeFile(fileName, buffer);
+  return fileName;
+}
+
+
 (async () => {
   // Convert SVG to PNG
   console.log(chalk.blue('Reading file...'));
   const file = await fs.readFile('source.svg')
   console.log(chalk.blue('Converting file to PNG...'));
-  const buffer = await svgToImg.from(file).toPng();
-  console.log(chalk.blue('Saving PNG...'));
-  await fs.writeFile('output.png', buffer);
+
+  // 512
+  await savePng(file, 512);
+  const smallFileName = await savePng(file, 48)
 
   // Convert PNG to ICO
   console.log(chalk.blue('Converting PNG to ICO...'));
-  const ico = await pngToIco('output.png');
+  const ico = await pngToIco(smallFileName);
   console.log(chalk.blue('Saving ICO...'));
   await fs.writeFile('output.ico', ico);
   console.log(chalk.green('Done!'));
