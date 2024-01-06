ced9a7adce3eda1ab5b22adfe9cbb40aa436c758
Carles Andres <carlos.andres.c@gmail.com>
2024-01-06 22:11:48 +0000

    Added all basic favicon sizes

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ import svgToImg from 'svg-to-img';
 import pngToIco from 'png-to-ico';
 import chalk from 'chalk';
 
-const savePng = async (file, size) => {
+const savePng = async (file, size, fileName) => {
   console.log(chalk.blue('Saving PNG size: ' + size));
   const buffer = await svgToImg.from(file).toPng({ width: size, height: size });
   console.log(chalk.blue('Saving PNG...'));
-  const fileName = `output-${size}.png`;
+  fileName = fileName || `output-${size}`;
+  fileName += '.png';
   await fs.writeFile(fileName, buffer);
   return fileName;
 }
@@ -20,13 +21,17 @@ const savePng = async (file, size) => {
   console.log(chalk.blue('Converting file to PNG...'));
 
   // 512
-  await savePng(file, 512);
+  await savePng(file, 180, 'apple-touch-icon');
+  await savePng(file, 192, 'android-chrome-192x192');
+  await savePng(file, 512, 'android-chrome-512x512');
   const smallFileName = await savePng(file, 48)
 
   // Convert PNG to ICO
   console.log(chalk.blue('Converting PNG to ICO...'));
   const ico = await pngToIco(smallFileName);
   console.log(chalk.blue('Saving ICO...'));
-  await fs.writeFile('output.ico', ico);
+  await fs.writeFile('favicon.ico', ico);
+  console.log(chalk.blue('Removing temp files...'));
+  await fs.unlink(smallFileName);
   console.log(chalk.green('Done!'));
 })();
